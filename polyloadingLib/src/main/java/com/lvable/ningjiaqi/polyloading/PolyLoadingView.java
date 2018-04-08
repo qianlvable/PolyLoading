f935155a768dccdf761a02ba7c2b53a563bca77b
qianlvable <wednesday114@gmail.com>
2018-04-08 11:45:30 +0800

    升级gradle

--- polyloadingLib/src/main/java/com/lvable/ningjiaqi/polyloading/PolyLoadingView.java
+++ polyloadingLib/src/main/java/com/lvable/ningjiaqi/polyloading/PolyLoadingView.java
@@ -8,7 +8,6 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import com.facebook.rebound.SimpleSpringListener;
